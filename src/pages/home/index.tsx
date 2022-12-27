78f7967596f8cf53ee28933029b7790b2678e3f3
Zenky <bzenky@gmail.com>
2022-12-27 11:47:35 -0300

    feat: static home ClaimUsernameForm component

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,6 +1,7 @@
 import { Heading, Text } from '@ignite-ui/react'
 import Image from 'next/image'
 import previewImage from '../../assets/app-preview.png'
+import { ClaimUserNameForm } from './components/ClaimUsernameForm'
 import { Container, Hero, Preview } from './styles'
 
 export default function Home() {
@@ -10,10 +11,13 @@ export default function Home() {
         <Heading as="h1" size="4xl">
           Agendamento descomplicado
         </Heading>
+
         <Text size="xl">
           Conecte seu calendário e permita que as pessoas marquem agendamentos
           no seu tempo livre.
         </Text>
+
+        <ClaimUserNameForm />
       </Hero>
 
       <Preview>
