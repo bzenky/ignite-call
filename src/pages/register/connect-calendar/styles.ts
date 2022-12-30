74af643c46d67b7bf71e07533cfba08159e41939
Zenky <bzenky@gmail.com>
2022-12-30 17:47:43 -0300

    feat: auth validation

--- src/pages/register/connect-calendar/styles.ts
+++ src/pages/register/connect-calendar/styles.ts
@@ -1,4 +1,4 @@
-import { Box, styled } from '@ignite-ui/react'
+import { Box, Text, styled } from '@ignite-ui/react'
 
 export const ConnectBox = styled(Box, {
   display: 'flex',
@@ -11,8 +11,13 @@ export const ConnectItem = styled('div', {
   alignItems: 'center',
   justifyContent: 'space-between',
   padding: '$4 $6',
-  marginBottom: '$2',
+  marginBottom: '$4',
 
   border: '1px solid $gray600',
   borderRadius: '$md',
+})
+
+export const AuthError = styled(Text, {
+  color: '#f75a78',
+  marginBottom: '$2',
 })
