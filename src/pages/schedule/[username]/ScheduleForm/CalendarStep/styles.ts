f584eb17e742134f29cbbd1d5bcba4a0ba30300d
Zenky <bzenky@gmail.com>
2023-01-05 19:49:23 -0300

    feat: calendar component structure

---
+++ src/pages/schedule/[username]/ScheduleForm/CalendarStep/styles.ts
@@ -0,0 +1,9 @@
+import { Box, styled } from '@ignite-ui/react'
+
+export const Container = styled(Box, {
+  position: 'relative',
+  display: 'grid',
+  margin: '$6 auto 0',
+  padding: 0,
+  maxWidth: '100%',
+})
