b8bd5d7dec28c433bd7a72d47a088a2f42c9ea87
Zenky <bzenky@gmail.com>
2023-01-06 10:20:14 -0300

    style: calendar component

--- src/components/Calendar/index.tsx
+++ src/components/Calendar/index.tsx
@@ -48,7 +48,7 @@ export function Calendar() {
               <CalendarDay>1</CalendarDay>
             </td>
             <td>
-              <CalendarDay>2</CalendarDay>
+              <CalendarDay disabled>2</CalendarDay>
             </td>
             <td>
               <CalendarDay>3</CalendarDay>
--- src/pages/schedule/[username]/ScheduleForm/CalendarStep/styles.ts
+++ src/pages/schedule/[username]/ScheduleForm/CalendarStep/styles.ts
@@ -6,4 +6,7 @@ export const Container = styled(Box, {
   margin: '$6 auto 0',
   padding: 0,
   maxWidth: '100%',
+
+  width: 540,
+  gridTemplateColumns: '1fr',
 })
